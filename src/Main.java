8304161f65d534eb4fed0a949e2638c622059dae
cmdijamco <201911137@fit.edu.ph>
2021-07-06 21:01:10 +0800

    Gaussian Calculator Done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-public class Main {
+import java.util.Arrays;
+
+public class Main{
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        InputWindow window = new InputWindow();
     }
 }
     
